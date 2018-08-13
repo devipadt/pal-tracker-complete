e200f3ed5968022ff074c113a1e612703c463d70
PAL-Labs <devipad.tripathy@hcl.com>
2018-08-13 18:21:09 -0300

    Added Test

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -2,12 +2,23 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
+    public String getMessage() {
+        return message;
+    }
+
+    private String message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return getMessage();
     }
 }
